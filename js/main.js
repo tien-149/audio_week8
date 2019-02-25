b3e73b980df02035059eaecfcf5a25f52bda6171
Le <t_le86524@tss-hr420-dm18.fc.ca>
2019-02-25 15:46:08 -0500

    The second change in class

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 		play = document.querySelector('#playButton'),
 		pause = document.querySelector('#pauseButton'),
 		rewind = document.querySelector('#rewindButton');
+		tracks = document.querySelectorAll('.trackholder');
 		//WWrite the function for the audio element
 		
 	function playAudio() {
@@ -14,11 +15,20 @@
 	function rewindAudio() {
 		aud.currentTime = 0;
 	}
+	function swapSource() {
+		let currentTrack = this.dataset.currenttrack;
+
+		aud.src = `audio/${currentTrack}`;
+		aud.load();
+		aud.play();
+	}
 
  //add event handling
  
  	play.addEventListener("click", playAudio);
  	pause.addEventListener("click", pauseAudio);
  	rewind.addEventListener("click", rewindAudio);
+
+ 	tracks.forEach(track => track.addEventListener("click", swapSource));
 
 })();
